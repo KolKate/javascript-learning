ecfd1df1464f3b10596ab6f4f8a5945bb9e5ada8
Катерина Тодорюк < >
2017-03-19 19:29:23 +0200

    change js

--- flex/js/main.js
+++ flex/js/main.js
@@ -4,10 +4,19 @@ window.onload = function(){
 	title.innerHTML = 'Hello';
 	console.info(title.innerHTML);
 
-setTimeout(function(){
+/*setTimeout(function(){
 	console.info('title change')
-	title.style.backgroundColor = '#blue';
+	title.style.backgroundColor = 'blue';
 }, 2000);
+*/
+	setInterval(changeColor, 1000);
+	title.style.backgroundColor = 'blue';
 
-
+	function changeColor(){
+		if (title.style.backgroundColor === 'blue') {
+         title.style.backgroundColor = 'red';
+         } else{
+         title.style.backgroundColor = 'blue';
+		}
+	}
 }
