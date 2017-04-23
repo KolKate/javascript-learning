722acaca395c770c9e36547bde8f640d615be002
Катерина Тодорюк < >
2017-04-23 20:32:34 +0300

    native edit todo(basic)

--- app-5-version/apps/native/js/app/todos/list-todos.js
+++ app-5-version/apps/native/js/app/todos/list-todos.js
@@ -47,8 +47,20 @@ ListTodos.prototype.getList = function() {
     var todoCellTextEl = markup.create({
       tag: 'td',
       parent: todoEl,
+    });
+
+    var todoSpanTextEl = markup.create({
+      tag: 'span',
+      parent: todoCellTextEl,
       content: todo.text,
       className: todo.checked ? 'checked' : ''
+    });
+
+    var todoInputTextEl = markup.create({
+      tag: 'input',
+      parent: todoCellTextEl,
+      attrs: [{placeholder: todo.text}],
+      className: 'hidden'
     });
 
     var todoCellActionsEl = markup.create({
@@ -65,6 +77,16 @@ ListTodos.prototype.getList = function() {
       parent: todoCellCheckboxEl
     });
 
+    var todoActionEditEl = markup.create({
+      tag: 'button',
+      attrs: [
+        { type: 'button' }
+      ],
+      className: 'btn btn-danger btn-xs',
+      content: '<span class="glyphicon glyphicon-pencil"></span>',
+      parent: todoCellActionsEl
+    });
+
     var todoActionDeleteEl = markup.create({
       tag: 'button',
       attrs: [
@@ -77,39 +99,70 @@ ListTodos.prototype.getList = function() {
 
     events.on(todoCellCheckboxEl, 'click', function(event) {
       event.preventDefault();
-      self.doCheck(todoEl, todoCheckboxEl, todoCellTextEl, todos, todo, index);
+      self.doCheck(todoEl, todoCheckboxEl, todoSpanTextEl, todos, todo, index);
     });
 
-    events.on(todoCellTextEl, 'click', function(event) {
-      event.preventDefault();
-      self.doCheck(todoEl, todoCheckboxEl, todoCellTextEl, todos, todo, index);
-    });
+    
 
     events.on(todoActionDeleteEl, 'click', function(event) {
       self.delete(index, todos);
     });
+
+    events.on(todoActionEditEl, 'click', function(event) {
+      event.preventDefault();
+      self.edit(todoInputTextEl, todoSpanTextEl, todo, todoActionEditEl, todos,
+  index);
+    });
   });
+};
+
+ListTodos.prototype.edit = function(
+  todoInputTextEl,
+  todoSpanTextEl,
+  todo,
+  todoActionEditEl,
+  todos,
+  index
+  ){
+  var toChange = todoInputTextEl.className === 'hidden';
+  var buttonSpan = todoActionEditEl.querySelector('span');
+  if(toChange){
+    todoSpanTextEl.className = 'hidden ' + (todo.checked ? 'checked' : '');
+    todoInputTextEl.className = '';
+    buttonSpan.className = 'glyphicon glyphicon-ok-sign';
+  } else {
+    //save
+    todoInputTextEl.className = 'hidden'; 
+    todoSpanTextEl.className = todo.checked ? 'checked' : '';
+    buttonSpan.className = 'glyphicon glyphicon-pencil';
+    todos[index].text = todoInputTextEl.value;
+    data.update('todos', JSON.stringify(todos));
+    events.send('get-todos-list');
+  }
+
+  
+
 };
 
 ListTodos.prototype.doCheck = function(
   todoEl,
   todoCheckboxEl,
-  todoCellTextEl,
+  todoSpanTextEl,
   todos,
   todo,
   index
 ) {
   var isChecked = todoCheckboxEl.className === 'glyphicon glyphicon-check';
-
+  var isSpanHidden = todoSpanTextEl.className.indexOf('hidden') != -1;
   if (isChecked) {
     todoEl.className = '';
     todoCheckboxEl.className = 'glyphicon glyphicon-unchecked';
-    todoCellTextEl.className = '';
+    todoSpanTextEl.className = isSpanHidden ? 'hidden' : '';
     todo.checked = false;
   } else {
     todoEl.className = 'success';
     todoCheckboxEl.className = 'glyphicon glyphicon-check';
-    todoCellTextEl.className = 'checked';
+    todoSpanTextEl.className = 'checked ' + (isSpanHidden ? 'hidden' : '');
     todo.checked = true;
   }
 
